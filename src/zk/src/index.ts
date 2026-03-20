53b3007a575e7beb3091e33c7b06e9e0a3fe3a96
AgustinBadi <librenotgratis@tuta.io>
2026-03-20 17:35:17 -0300

    feat(zk): add options encoding functions

--- src/zk/src/index.ts
+++ src/zk/src/index.ts
@@ -1,5 +1,5 @@
 // Re-exports all public symbols from the src/zk module.
 export * from './conversion.js';
-// export * from './signal.js'; // not yet implemented
+export * from './signal.js';
 export * from './mpf.js';
 export * from './proof.js';
